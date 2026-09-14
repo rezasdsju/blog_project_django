2c09773642e377e8d6151c76e59a8c2dad0f660a
rezasdsju <rezaul2002104@gmail.com>
2026-09-14 08:51:42 +0600

    feat: add label to email field

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(label='Email Address', required=True)
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
