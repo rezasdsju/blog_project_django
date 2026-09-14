651480f53bb5fc1d8ab1772afc04ed23afa5c631
rezasdsju <rezaul2002104@gmail.com>
2026-09-14 09:01:33 +0600

    feat: add user profile page

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -5,5 +5,6 @@ app_name = 'App_Login'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/',views.login_page,name='login'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name= 'profile')
 ]
--- App_Login/views.py
+++ App_Login/views.py
@@ -35,3 +35,11 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+
+
+
+@login_required
+def profile(request):
+    return render(request, 'App_Login/profile.html', context={})
