8ea12ae060b13eae28a0d7eeb80f1de93ad84474
rezasdsju <rezaul2002104@gmail.com>
2026-09-14 07:48:38 +0600

    feat: configure login functionality

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from App_Login import views
 app_name = 'App_Login'
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('login/',views.login_page,name='login'),
 ]
